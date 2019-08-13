d40062f7b08b5e0889e47fbaeec2851fced894a4
CrowCakes <jeremychoa@gmail.com>
2019-08-13 08:56:59 +0800

    New scene added for Main Menu

---
+++ Assets/Scripts/UI/RoguelikeButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RoguelikeButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Text description;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadRoguelike()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        description.text = "Traverse an infinite catacomb and explore as many rooms as possible";
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        description.text = "";
+    }
+}
---
+++ Assets/Scripts/UI/SandboxButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SandboxButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Text description;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadSandbox() {
+        SceneManager.LoadScene("sandbox");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        description.text = "Collect the coin in the middle of the tiny castle";
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        description.text = "";
+    }
+}
